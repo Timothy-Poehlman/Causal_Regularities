1a6b13cc1423e5c8762236f079a23caae9e9ee72
Tim P <timothypoehlman@gmail.com>
2021-05-21 16:02:30 -0700

    added linked list

--- C_Algorithm/pairlist.h
+++ C_Algorithm/pairlist.h
@@ -4,6 +4,21 @@ struct pair_desc
     int index,value;
 } pair_desc;
 
+typedef struct pair_list *PairList;
+struct pair_list
+{
+    Pair* list;
+    int size;
+    PairList next;
+} pair_list;
+
+typedef struct condition_list *ConditionList;
+struct condition_list
+{
+    PairList list;
+    int size;
+} condition_list;
+
 Pair make_pair(int index, int value);
 int pairs_equal(Pair p1, Pair p2);
 int pairInList(Pair* pairList, Pair pair, int rows, int cols);
